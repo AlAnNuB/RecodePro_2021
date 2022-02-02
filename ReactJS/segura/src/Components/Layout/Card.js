749067bfa5994d2fa2528c5392f5cea844d43a9c
AlAnNuB <alannub6@gmail.com>
2022-02-01 21:55:11 -0300

    alt com props nome

--- ReactJS/segura/src/Components/Layout/Card.js
+++ ReactJS/segura/src/Components/Layout/Card.js
@@ -2,7 +2,7 @@ function Card(props) {
   return(
     <div className="col-12 col-md-6 col-lg-4 col-xl-3">
       <div className="card destinos m-2 p-0">
-        <img src={props.img} className="card-img-top h-60 m-0 p-0" alt="Alagoas" />
+        <img src={props.img} className="card-img-top h-60 m-0 p-0" alt={props.nome} />
         <div className="card-body">
           <h4 className="card-title">{props.nome}</h4>
           <p className="card-text small m-0">Ida: {props.ida}</p>
--- ReactJS/segura/src/Components/Layout/CardDesconto.js
+++ ReactJS/segura/src/Components/Layout/CardDesconto.js
@@ -2,7 +2,7 @@ function CardDesconto(props) {
   return(
     <div class="col-12 col-md-6 col-lg-4 col-xl-3">
       <div class="card destinos m-2 p-0">
-        <img src={props.img} class="card-img-top h-60 m-0 p-0" alt="Alagoas" />
+        <img src={props.img} class="card-img-top h-60 m-0 p-0" alt={props.nome} />
         <div class="card-body">
           <h4 class="card-title">{props.nome}</h4>
           <p class="card-text small m-0">Ida: {props.ida}</p>
